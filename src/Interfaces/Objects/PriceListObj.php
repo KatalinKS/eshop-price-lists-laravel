24a390ebe68e1d64763306fca454c8f8368dec68
Skej <konstantin@katalin.ru>
2021-10-12 17:35:31 +0300

    добавлен в интерфейс метод получения емейлов для рассылки для заказов

--- src/Interfaces/Objects/PriceListObj.php
+++ src/Interfaces/Objects/PriceListObj.php
@@ -2,7 +2,16 @@
 
 namespace KatalinKS\PriceList\Interfaces\Objects;
 
+use Illuminate\Support\Collection;
+
 interface PriceListObj
 {
     public function getId(): int;
+
+
+    /**
+     * Возвращает коллекцию емейлов для оформленных заказов
+     * @return Collection
+     */
+    public function getOrdersEmails(): Collection;
 }
